28fb4ebeee7a4674bfac88eb0221d82e2a2fda0d
ermias <ermuka@gmail.com>
2022-12-08 07:24:03 -0800

    end

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
  * main - Entry
- * return: Always
+ * Return: Always 0 (Success)
 */
 int main(void)
 {
@@ -9,6 +9,6 @@ int main(void)
 	printf("Size of an int: %i byte(s)\n", sizeof(int));
 	printf("Size of a long int: %i byte(s)\n", sizeof(long int));
 	printf("Size of a long long int: %i byte(s)\n", sizeof(long long int));
-	printf("Size of a flloat: %i byte(s)\n", sizeof(float));
+	printf("Size of a float: %i byte(s)\n", sizeof(float));
 	return (0);
 }
