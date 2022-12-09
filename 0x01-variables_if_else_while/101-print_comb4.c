9846abd8a7cc9df4c0726db2dfb330b0ff0b3f41
ermias <ermuka@gmail.com>
2022-12-09 07:18:40 -0800

    threeDigits

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,35 @@
+#include <stdio>
+
+/**
+ * main - comboo of three digits
+ * Return: Always 0
+ */
+int main(void)
+{
+	int one;
+	int ten;
+	int hundred;
+
+	for (hundred = '0'; hundred <= '9'; hundred++)
+	{
+		for (ten = '0'; ten <= '9'; ten++)
+		{
+			for (one = '0'; one <= '9'; one++)
+			{
+				if (!((one == ten) || (ten == hundred) || (ten > one) || (hundred > ten)))
+				{
+					putchar(hundred);
+					putchar(ten);
+					putchar(one);
+					if (!(one == '9' && hundred == '7' && ten == '8'))
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
