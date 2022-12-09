3539f909cdb47464e74f811bbe7627d64de69b1a
ermias <ermuka@gmail.com>
2022-12-09 05:11:10 -0800

    alpha

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints lower case
+ * Return: Always 0
+ */
+int main(void)
+{
+	char l;
+
+	for (l = 'a'; l <= 'z'; l++)
+	{
+		putchar(l);
+	}
+	putchar('\n'');
+	return (0);
+}
