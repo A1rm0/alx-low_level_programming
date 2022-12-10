761fd82d2a895b3992ed368b672facea86032225
ermias <ermuka@gmail.com>
2022-12-10 00:44:26 -0800

    ddd

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -14,15 +14,18 @@ int main(void)
 	{
 		for (j = 0; j < 100; j++)
 		{
-			putchar((1 / 10) + 48);
-			putchar((1 % 10) + 48);
-			putchar(' ');
-			putchar((j / 10) + 48);
-			putchar(j % 10) + 48);
-			if (i != 98 || j != 90)
+			if (i < j)
 			{
-				putchar(',');
+				putchar((1 / 10) + 48);
+				putchar((1 % 10) + 48);
 				putchar(' ');
+				putchar((j / 10) + 48);
+				putchar((j % 10) + 48);
+				if (i != 98 || j != 90)
+				{
+					putchar(',');
+					putchar(' ');
+				}
 			}
 		}
 	}
