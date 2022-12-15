0483c9ddea055a3aa5225de94ac369222f03401c
ermias <ermuka@gmail.com>
2022-12-15 19:56:31 +0300

    ahunisbeza

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -14,16 +14,16 @@ void print_triangle(int size)
 		while (j < size - i)
 		{
 			_putchar(' ');
-			j++
+			j++;
 		}
 		j = 0;
 		while (j < i)
 		{
 			_putchar('#');
-			j++
+			j++;
 		}
 		_putchar('\n');
-		i++
+		i++;
 	}
 	if (i == 1)
 		_putchar('\n');
