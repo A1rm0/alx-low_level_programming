9a0112d0267cb6989e0089e97f8819692c060589
ermias <ermuka@gmail.com>
2022-12-15 19:27:01 +0300

    task9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,35 @@
+#include <stdio.>
+/**
+ * main - prints fizz and buzz
+ * Return: Always 0
+ */
+int int main(void)
+{
+	int n;
+
+	n = 1;
+	printf("%d", n);
+
+	for (n = 0; n <= 100; n++)
+	{
+		if ((n % 3 == 0) && (n % 5 == 0))
+		{
+			printf("FizzBuzz");
+		}
+		else if (n % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (n % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else
+		{
+			printf("%d", n);
+		}
+		/* code */
+	}
+	printf("\n");
+	return (0);
+}
